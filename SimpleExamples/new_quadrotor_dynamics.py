fixed up to integer multiplication overflow

SimpleExamples/new_quadrotor_dynamics.py
@@ -142,14 +142,14 @@ class QuadrotorDynamics:
 
     ## static methods ##
     @staticmethod
-    def compute_rotation_matrix(quat: Tensor) -> Tensor:
+    def compute_rotation_matrix(euler: Tensor) -> Tensor:
         """
         Compute the rotation matrix from roll, pitch, and yaw angles.
         :param quat:    Tensor containing the roll, pitch, and yaw angles for all batches
         :return:        Rotation matrix using these angles to transform from body to world frame
         """
-        batch = quat.shape[0]
-        roll, pitch, yaw = quat[:, 0], quat[:, 1], quat[:, 2]  # unpack angles
+        batch = euler.shape[0]
+        roll, pitch, yaw = euler[:, 0], euler[:, 1], euler[:, 2]  # unpack angles
 
         # precompute trigonometric results
         c_roll, s_roll = torch.cos(roll), torch.sin(roll)
@@ -206,22 +206,16 @@ class QuadrotorDynamics:
             torch.stack([q, -p, batch_zeros, r], dim=1),
             torch.stack([-p, -q, -r, batch_zeros], dim=1)
         ], dim=1) * 0.5  # shape (batch, 4, 4)
-        print(f"Shape of lambda: {lambda_.shape}")
         theta = omega_norm * time_step / 2
         # Intermediate calculation for calculating the next quaternion; Reshaping Tensors is another way to add singleton
         # dimensions while also explicitly listing the shapes. Singleton dimensions allow for broadcasting, i.e. the
         # torch.eye(4).reshape(1,4,4)*torch.cos(theta).reshape(num_update,1,1) term produces a Tensor that has shape
         # (num_update, 4, 4) where each 4x4 identity matrix is multiplied by its corresponding cos(theta) scalar value.
         inter_quat = torch.eye(4).reshape(1,4,4)*torch.cos(theta).reshape(num_update,1,1) + (2*torch.sin(theta)/omega_norm).reshape(num_update,1,1)*lambda_
-        return torch.cross(inter_quat, quat)
-        # # Formalize the next quaternion for the values that should be updated
-        # print("inter_quat size", inter_quat.size())
-        # print("quat size", quat.size())
-        # next_quat[update_mask, :] = torch.einsum('bmn,bn->bn', inter_quat, quat)
-        
-        #return next_quat
+        return torch.einsum('bmn, bn->bm', inter_quat, quat)
 
-    def quaternion_to_rotation_matrix(self, quat: Tensor) -> Tensor:
+    @staticmethod
+    def quaternion_to_rotation_matrix(quat: Tensor) -> Tensor:
         """
 
         :param quat:
@@ -249,7 +243,43 @@ class QuadrotorDynamics:
         ], dim=1)  # shape (batch, 3, 3)
 
         return rotation_matrix
+    
+    @staticmethod
+    def rotation_matrix_to_euler(R: Tensor)->Tensor:
+        """
+        Convert a 3x3 rotation matrix to Euler angles (roll, pitch, yaw).
+        Assumes XYZ rotation order.
 
+        Parameters:
+            R (numpy.ndarray): 3x3 rotation matrix.
+
+        Returns:
+            tuple: (roll, pitch, yaw) in radians.
+        """
+
+        pitch = torch.arcsin(-R[:, 2, 0])  # θ (Pitch)
+        
+        # if torch.abs(R[:, 2, 0]) != 1:  # Normal case (no gimbal lock)
+        roll = torch.arctan2(R[:, 2, 1], R[:, 2, 2])  # φ (Roll)
+        yaw = torch.arctan2(R[:, 1, 0], R[:, 0, 0])  # ψ (Yaw)
+        # else:  # Gimbal lock case
+        #     yaw = 0
+        #     roll = np.arctan2(-R[0, 1], R[1, 1])  # φ (Roll)
+        euler = torch.stack([roll, pitch, yaw], dim=1)
+        return euler
+
+    @staticmethod
+    def euler_to_quaternion(euler: Tensor) -> Tensor:
+        phi, theta, psi = euler[:, 0], euler[:, 1], euler[:, 2]  # roll, pitch, yaw
+        # print(f"phi: {phi.shape}, theta: {theta.shape}, psi: {psi.shape}")
+        w = torch.cos(phi / 2)*torch.cos(theta / 2)*torch.cos(psi/2) + torch.sin(phi/2)*torch.sin(theta/2)*torch.sin(psi/2)
+        x = torch.sin(phi / 2)*torch.cos(theta / 2)*torch.cos(psi/2) + torch.cos(phi/2)*torch.sin(theta/2)*torch.sin(psi/2)
+        y = torch.cos(phi / 2)*torch.sin(theta / 2)*torch.cos(psi/2) + torch.sin(phi/2)*torch.cos(theta/2)*torch.sin(psi/2)
+        z = torch.cos(phi / 2)*torch.cos(theta / 2)*torch.sin(psi/2) + torch.sin(phi/2)*torch.sin(theta/2)*torch.cos(psi/2)
+
+        quaternion = torch.stack([w, x, y, z], dim=1)
+        return quaternion
+    
     ## properties ##
     @property
     def x_equilibrium(self):

neural_lyapunov_training/dynamical_system.py
@@ -3,8 +3,7 @@ import torch.nn as nn
 from enum import Enum
 import numpy as np
 import control
-from new_quadrotor_dynamics import QuadrotorDynamics
-
+from SimpleExamples.new_quadrotor_dynamics import QuadrotorDynamics
 
 class DiscreteTimeSystem(nn.Module):
     """
@@ -141,7 +140,7 @@ class SecondOrderDiscreteTimeSystem(DiscreteTimeSystem):
         """
         Given the control gain K and observer gain L, solve the discrete-time Lyapunov equation
         for the closed-loop system with the states and controls at equilibrium.
-        The linearized dynamics are computed from the continuous-time system with Explicit Euler method.
+        The linearized dynamics are computed from the  continuous-time system with Explicit Euler method.
         """
         x0 = self.x_equilibrium.unsqueeze(0)
         Ad, Bd = self.continuous_time_system.linearized_dynamics(
@@ -181,8 +180,12 @@ class QuadrotorSystem(SecondOrderDiscreteTimeSystem):
         pre_mask = np.zeros(12)
         pre_mask[3:6] = 1
         self._a_mask = np.argwhere(pre_mask == 1).flatten().tolist()  # angular mask
-        self._r_mask = np.argwhere(pre_mask == 0).flatten().tolist()  # rest mask
-        
+        self._r_lower_mask = np.concatenate([
+            np.arange(0, 3)     # x,y,z positions
+        ]).tolist()
+        self._r_upper_mask = np.concatenate([
+            np.arange(6, 12)     # vx,vy,vz, rollpitchyaw positions
+        ]).tolist()
     """
     q = [position_x, position_y, position_z, roll, pitch, yaw]
     qdot = [velocity_x, velocity_y, velocity_z, roll rate, pitch rate, yaw rate]
@@ -228,27 +231,26 @@ class QuadrotorSystem(SecondOrderDiscreteTimeSystem):
         assert x.shape[0] == u.shape[0]
 
         a_mask = self._a_mask
-        r_mask = self._r_mask
-        quat = x[:, a_mask]  # Angles (orientation or quaternion)
+        r_upper_mask = self._r_upper_mask
+        r_lower_mask = self._r_lower_mask
+        euler = x[:, a_mask]  # Angles (orientation or quaternion)
         qddot = self.continuous_time_system.forward(x, u)
         angular_vel = qddot[:, a_mask]
-
-        print(f"angular_vel size {angular_vel.size()}")
-        print(f"a_mask: {a_mask}")
-        print(f"r_mask: {r_mask}")
-        print(f"quat size: {quat.size()}")
-        print(f"quat: {quat}")
-
+        quat = QuadrotorDynamics.euler_to_quaternion(euler)
         new_quat = QuadrotorDynamics.integrateQ(quat, angular_vel, self.dt)
         x_upper = x[:, self.nq:]  # current velocities
         x_lower = x[:, :self.nq]  # current positions
 
         q_next, qdot_next = torch.zeros_like(x[:, : self.nq]), torch.zeros_like(x[:, : self.nq])
-        q_next[:, a_mask] = new_quat
+        # gets the rotation matrix from the new quaternion
+        rotation_matrix = QuadrotorDynamics.quaternion_to_rotation_matrix(new_quat)
+        # from the new rotation matrix, get the new Euler angles
+        new_euler = QuadrotorDynamics.rotation_matrix_to_euler(rotation_matrix)
+        q_next[:, a_mask] = new_euler
 
         # update the velocities (qdot_next)
         if self.velocity_integration == IntegrationMethod.ExplicitEuler:
-            qdot_next = x_upper + qddot * self.dt
+            qdot_next = x_upper + qddot[:, :6] * self.dt
         else:
             raise NotImplementedError
         
@@ -260,15 +262,21 @@ class QuadrotorSystem(SecondOrderDiscreteTimeSystem):
         # else:
         #     raise NotImplementedError
         
-        # update the positions (q_next)
-        x_upper_rest = x_upper[:, r_mask]
-        x_lower_rest = x_lower[:, r_mask]
+        x_velocities = x_upper[:, :3]
+        
+       
+        x_positions = x_lower[:, :3]
+
+
+        self.position_integration = IntegrationMethod.MidPoint
         if self.position_integration == IntegrationMethod.MidPoint:
-            qdot_next_rest = qdot_next[:, r_mask]
-            q_next_rest = x_lower_rest + (qdot_next_rest + x_upper_rest) / 2 * self.dt
-            q_next[:, r_mask] = q_next_rest
+            # lower
+            qdot_next_rest = qdot_next[:, :3]
+            q_next_rest = x_velocities + (qdot_next_rest + x_positions) / 2 * self.dt
+            q_next[:, :3] = q_next_rest
         elif self.position_integration == IntegrationMethod.ExplicitEuler:
-            q_next_rest = x_lower_rest + x_upper_rest * self.dt
+            # upper
+            q_next_rest = x_velocities + x_positions * self.dt
             q_next[:, r_mask] = q_next_rest
         else:
             raise NotImplementedError

neural_lyapunov_training/train_utils.py
@@ -237,7 +237,8 @@ def compute_sample_loss(
         sample_output = torch.clamp(-lya_sample, min=0.0)
         sample_loss = sample_output.sum()  # max()
         max_sample_violation = sample_output.max().item()
-        unsatisfied_sample = (lya_sample < 0).abs().sum().item()
+        unsatisfied_sample = (lya_sample < 0).sum().item()
+        #FIXME: Check the intent of unsatisifed_sample (should it be sum or abs.sum?)
     else:
         max_sample_violation = unsatisfied_sample = 0
         sample_loss = torch.tensor(0, device=device)