ca2e1960dcfded3542e0011340f46eff672edf4a
jorgejc2 <jorgejc2@illinois.edu>
2025-03-16 16:59:22 -0500

    first implementation of NN Zubov's PDE

--- SimpleExamples/zubov/architecture.py
+++ SimpleExamples/zubov/architecture.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
-from torch import Tensor, optim, tanh
+from torch import Tensor, optim
 from collections import OrderedDict
-from typing import Tuple, Union, Optional, Dict, List
+from typing import Tuple, Union, Optional, Dict, List, Callable
 
 to_numpy = lambda x : x.detach().cpu().numpy() if isinstance(x, Tensor) else x
 
@@ -10,7 +10,7 @@ class ZubovNetwork(nn.Module):
 
     def __init__(self, dynamical_system, lambda_b: float, lb: Tensor, ub: Tensor, alpha: float, width: int,
                  n_layers: int, input_dim: int, lrate: float,
-                 horizon:float=1200, weight_decay:float=1e-5):
+                 horizon:float=1200, weight_decay:float=1e-5, clip_gradient_norm: Optional[float] = None):
         """
 
         :param dynamical_system:
@@ -47,13 +47,30 @@ class ZubovNetwork(nn.Module):
         self.lambda_b = lambda_b
         self.lb = lb
         self.ub = ub
+        self.input_dim = input_dim
         self.alpha = alpha
         self.horizon = horizon
         self.num_steps = int(horizon / self.dynamical_system.dt)
         self.optimizer = optim.Adam(self.model.parameters(), lr=lrate, weight_decay=weight_decay)
+        self.clip_gradient_norm = clip_gradient_norm
 
     def forward(self, x: Tensor) -> Tensor:
         return self.model(x)
+
+    def forward_with_coords(self, x: Tensor) -> Tuple[Tensor, Tensor]:
+        """
+
+        Before the forward pass, clone the input and enable its gradient. Returning this cloned input allows the
+        output of the network to be differentiated w.r.t. the input.
+
+        :param x: (batches, input_dim)
+        :return:
+        """
+        x = x.clone().detach().requires_grad_(True)  # allows to take derivative w.r.t. input
+
+        output = self.forward(x)
+
+        return output, x
 
     @staticmethod
     def maximal_lyapunov_fn(traj: Tensor) -> Tensor:
@@ -76,7 +93,7 @@ class ZubovNetwork(nn.Module):
         :return:
         """
         traj_norm = torch.linalg.vector_norm(traj, dim=2)
-        lya_discrete = traj_norm.sum(dim=1)
+        lya_discrete = traj_norm.sum(dim=1, keepdim=True)
 
         return lya_discrete
 
@@ -90,12 +107,12 @@ class ZubovNetwork(nn.Module):
         :return:
         """
 
-        # reshape and broadcast limits along batch dimension
+        # For points inside the box, project to the nearest boundary
         x_L = x_L.reshape(1, -1).expand(x.shape[0], -1)
         x_U = x_U.reshape(1, -1).expand(x.shape[0], -1)
-        b = torch.arange(x.shape[0])  # used for indexing
+        b = torch.arange(x.shape[0])
 
-        x = torch.clamp(x, min=x_L, max=x_U)
+        # x = torch.clamp(x, min=x_L, max=x_U)
         min_l, min_l_idx = torch.min((x - x_L).abs(), dim=1)
         min_u, min_u_idx = torch.min((x - x_U).abs(), dim=1)
 
@@ -108,7 +125,7 @@ class ZubovNetwork(nn.Module):
         return x
 
 
-    def step(self, x: Tensor) -> Tuple[float, List[float]]:
+    def step(self, x: Tensor, x_b: Tensor, u: Callable) -> Tuple[float, List[float]]:
         """
 
         :param x: (batch_size, input_dim)
@@ -120,36 +137,105 @@ class ZubovNetwork(nn.Module):
                 grad_outputs = torch.ones_like(y)
             grad = torch.autograd.grad(y, [x], grad_outputs=grad_outputs, create_graph=True)[0]
             return grad
-        psi = lambda t : torch.linalg.vector_norm(t, dim=1)
+
+        psi = lambda t : torch.linalg.vector_norm(t, dim=1, keepdim=True)
 
         # Get points on the border of the region of interest, ∂R₂ where R₂ = {αx : x ∈ R₁},
         # R₁ ⊆ 𝒟, and 𝒟 is the region from which we are drawing samples.
-        x_b = ZubovNetwork.project_to_box_border(self.alpha*x, self.lb, self.ub)
+        # x_b = ZubovNetwork.project_to_box_border(self.alpha*x, self.lb, self.ub)
+
+        with torch.no_grad():
+            # in case dynamical system is described as another NN, we do not want to update its parameter
+            # fx = self.dynamical_system(x)  # return the next state
+            traj = self.dynamical_system(x, u, self.num_steps)  # return the trajectory
+            fx = traj[:, 0, :]  # get the next state
+            max_v = ZubovNetwork.maximal_lyapunov_fn(traj)
+            psi_output = psi(x)
 
         # zero the gradients
         self.optimizer.zero_grad()
 
-        yhat = self.forward(x)  # zubov output
-        grad_output = _gradient(x, yhat) # grad of zubov w.r.t. x
+        yhat, coords = self.forward_with_coords(x)  # zubov output
 
-        with torch.no_grad():
-            # in case dynamical system is described as another NN, we do not want to update its parameters
-            fx = self.dynamical_system(x)  # return the next state
-            traj = self.dynamical_system(x, self.num_steps)  # return the trajectory
+        grad_output = _gradient(coords, yhat) # grad of zubov w.r.t. x
 
-        max_v = ZubovNetwork.maximal_lyapunov_fn(traj)
-
-        loss_z = (self.forward(torch.zeros((1, self.input_dim)))**2).squeeze(0)
-        loss_r = torch.linalg.vector_norm((yhat - self.alpha * max_v)**2, dim=0, ord=1)
-        loss_p1 = torch.einsum('bi,bi->b', grad_output, fx)
-        loss_p2 = self.alpha * (1 - yhat) * (1 + yhat) * psi(x)
-        loss_p = torch.linalg.vector_norm((loss_p1 + loss_p2)**2, dim=0, ord=1)
-        loss_b = self.lambda_b * torch.linalg.vector_norm((self.forward(x_b)).abs(), dim=0, ord=1)
-
+        # Loss Z ensures that at the equilibrium, the output of W(x) is 0.
+        loss_z = (self.forward(torch.zeros((1, self.input_dim)))**2)
+        # Loss R ensures that W(x) = tanh(a*V(x)) where V(x) in this implementation is
+        # an approximation of the defined Maximal Lyapunov function
+        loss_r = (yhat - torch.tanh(self.alpha * max_v))**2
+        # Loss P is the derivative condition: ∂ₓW(x)ᵀf(x) = a(1−W(x))(1+W(x))Φ(x)
+        # where Φ(x) := ‖x‖
+        loss_p1 = torch.einsum('bi,bi->b', grad_output, fx).unsqueeze(1)
+        loss_p2 = self.alpha * (1 - yhat) * (1 + yhat) * psi_output
+        loss_p = (loss_p1 + loss_p2)**2
+        loss_b = self.lambda_b * (self.forward(x_b) - 1).abs()
         loss = loss_z + loss_r + loss_p + loss_b
+        total_loss = loss.mean()
 
         # perform backward gradient calculations
-        loss.backward()
+        total_loss.backward()
 
-        return loss.item(), [t.item() for t in [loss_z, loss_r, loss_p]]
+        # perform gradient clipping
+        # typically recommended for stable training
+        if self.clip_gradient_norm is not None:
+            nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=self.clip_gradient_norm)
 
+        # update the model parameters
+        self.optimizer.step()
+
+        rest_losses = [t.mean().item() if isinstance(t, Tensor) else t for t in [loss_z, loss_r, loss_p, loss_b]]
+        return total_loss.item(), *rest_losses
+
+
+if __name__ == "__main__":
+    # FIXME: Clean up, solely for debugging
+
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as patches
+
+    to_numpy = lambda x: x.detach().cpu().numpy()
+
+
+    def plot_2d_box(ax, x_L, x_U):
+        width = x_U[0] - x_L[0]
+        height = x_U[1] - x_L[1]
+
+        rect = patches.Rectangle(x_L, width, height, linewidth=2, edgecolor='blue', facecolor='none')
+        ax.add_patch(rect)
+        ax.set_xlim(x_L[0] - 1, x_U[0] + 1)
+        ax.set_ylim(x_L[1] - 1, x_U[1] + 1)
+        ax.set_aspect('equal', adjustable='box')
+        ax.grid(True)
+    pendulum_continuous = lambda x : x
+    x_L = -torch.ones((2,))
+    x_U = torch.ones((2,))
+    # zubov_args = {
+    #     "dynamical_system": pendulum_continuous,
+    #     "lambda_b": 0.01,
+    #     "lb": x_L,
+    #     "ub": x_U,
+    #     "alpha": 0.2,
+    #     "width": 32,
+    #     "n_layers": 5,
+    #     "input_dim": 2,
+    #     "lrate": 0.01,
+    #     "horizon": 20,
+    #     "weight_decay": 1e-5
+    # }
+    # zubov_network = ZubovNetwork(**zubov_args)
+    x = torch.rand(2048, 2)
+    x_new = ZubovNetwork.project_to_box_border(x, x_L, x_U)
+
+    x_np = to_numpy(x)
+    x_new_np = to_numpy(x_new)
+    print(f"x: {x_np}")
+    print(f"x_new: {x_new_np}")
+
+    fig, ax = plt.subplots()
+    print(ax)
+    plot_2d_box(ax, x_L, x_U)
+    ax.scatter(x_np[:, 0], x_np[:, 1], color='red', label='Original Points')
+    ax.scatter(x_new_np[:, 0], x_new_np[:, 1], color='green', label='Projected Points')
+    ax.legend()
+    plt.show()
--- SimpleExamples/zubov/dataset.py
+++ SimpleExamples/zubov/dataset.py
@@ -8,17 +8,75 @@ from numpy import ndarray
 to_numpy = lambda x : x.detach().cpu().numpy()
 
 class SampleDataset(Dataset):
-    def __init__(self, n_samples: int, lb: Tensor, ub: Tensor, alpha: float):
+    def __init__(self, n_samples: int, batch_size: int, lb: Tensor, ub: Tensor, alpha: float, verbose:bool=True):
         super().__init__()
         self.n_samples = n_samples
-        self.lb = lb
-        self.ub = ub
+        self.batch_size = batch_size
+        self.lb = lb.reshape(1, -1)
+        self.ub = ub.reshape(1, -1)
+        self.input_dim = len(lb)
         self.alpha = alpha
         self.box_range = self.ub - self.lb
+        # self.randomize_samples()
+        if verbose:
+            print(f"n_samples = {n_samples}, batch_size = {batch_size}")
+            print(f"lb shape = {self.lb.shape}, ub shape = {self.ub.shape}")
+            print(f"box_range shape = {self.box_range.shape}")
+            print(f"input_dim = {self.input_dim}")
 
     def __len__(self) -> int:
-        return self.n_samples
+        return self.n_samples // self.batch_size
 
-    def __getitem__(self, idx: int) -> Tensor:
-        x = torch.rand((self.n_samples, len(self.lb)))*self.box_range + self.lb
+    def __getitem__(self, idx: int):
+        return self.randomize_samples(self.batch_size)
+        # return self.x[idx*self.batch_size:(idx+1)*self.batch_size], self.x_b[idx*self.batch_size:(idx+1)*self.batch_size]
+
+    @staticmethod
+    def project_to_box_border(x: Tensor, x_L: Tensor, x_U: Tensor) -> Tensor:
+        """
+
+        :param x:       box samples
+        :param x_L:     lower limit box to project onto
+        :param x_U:     upper limit box to project onto
+        :return:
+        """
+
+        # For points inside the box, project to the nearest boundary
+        x_L = x_L.reshape(1, -1).expand(x.shape[0], -1)
+        x_U = x_U.reshape(1, -1).expand(x.shape[0], -1)
+        b = torch.arange(x.shape[0])
+
+        # x = torch.clamp(x, min=x_L, max=x_U)
+        min_l, min_l_idx = torch.min((x - x_L).abs(), dim=1)
+        min_u, min_u_idx = torch.min((x - x_U).abs(), dim=1)
+
+        mask = min_l < min_u
+        min_idx = torch.where(mask, min_l_idx, min_u_idx)
+        border = torch.where(mask, x_L[b, min_l_idx], x_U[b, min_u_idx])
+
+        x[b, min_idx] = border  # perform projection
+
         return x
+
+    def randomize_samples(self, num_samples: int):
+        x = torch.rand((num_samples, self.input_dim)) * self.box_range + self.lb
+        x_b = SampleDataset.project_to_box_border(self.alpha * x, self.lb, self.ub)
+        return x, x_b
+
+if __name__ == "__main__":
+    from torch.utils.data import DataLoader
+
+    batch_size = 2048
+    x_L = -torch.ones(2)
+    x_U = torch.ones(2)
+    dataset_args = {
+        "n_samples": 10_000,
+        "batch_size": batch_size,
+        "lb": x_L,
+        "ub": x_U,
+        "alpha": 0.2,
+    }
+    zubov_dataset = SampleDataset(**dataset_args)
+    train_loader = DataLoader(zubov_dataset, batch_size=1, shuffle=True, drop_last=True)
+    for batch_x in train_loader:
+        print(f"batch_x shape = {batch_x.shape}")
